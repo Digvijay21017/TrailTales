7109c196d6532ee48e5764446149eaa3dee936fc
Digvijay <digvijayghule2001@gmail.com>
2025-04-14 22:53:10 +0530

    Added docker files

--- Backend/index.js
+++ Backend/index.js
@@ -19,7 +19,7 @@ app.use('/user', userRoutes);
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.CONNECTION_URL)
-    .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
+    .then(() => app.listen(PORT, '0.0.0.0' , () => console.log(`Server running on port ${PORT}`)))
     .catch((error) => console.log(error.message))
 
 
